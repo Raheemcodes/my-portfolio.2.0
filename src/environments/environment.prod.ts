e591fa6304a0d16118c9ebae376f42c2d536b74f
Raheemcodes <raheemscorporatemail@gmail.com>
2023-08-10 21:19:13 +0100

    Change API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   frontendAddress: 'https://yraheem.tech',
-  REST_API: 'https://raheem-portfolio-api.onrender.com',
+  REST_API:
+    'https://b70gewuor6.execute-api.us-east-1.amazonaws.com/porfolio-restAPI',
 };
